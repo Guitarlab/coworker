4b5fb31197ae63e547d443f7ab76afd7087c6aec
guitarton <guitarton@yandex.ru>
2016-04-28 21:48:04 +0300

    get token

--- coworker/views.py
+++ coworker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
 
 
 # Create your views here.
@@ -12,6 +14,9 @@ def index(request):
 @login_required
 def profile(request):
     current_user = request.user
-    id = current_user.id
+    github_socialapp = SocialApp.objects.get(name='github app')
+    current_socialaccount = SocialAccount.objects.get(user=current_user)
+    current_token = SocialToken.objects.get(app=github_socialapp,
+                                            account=current_socialaccount)
     print(current_user.id)
     return render(request, 'coworker/profile.html', {'id': id})
