40d06ebd30b56343bf291f0a26c22142c494660d
guitarton <guitarton@yandex.ru>
2016-04-28 17:04:55 +0300

    show id

--- coworker/views.py
+++ coworker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    current_user = request.user
+    id = current_user.id
+    print(current_user.id)
+    return render(request, 'coworker/profile.html', {'id': id})  # Create your views here.
--- django_coworker/urls.py
+++ django_coworker/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from  coworker import views as coworker_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/profile/', coworker_views.profile, name='profile'),
 ]
