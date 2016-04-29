5b76592aa45dfbba3d0359a742c1175dcbc153e4
Evgeni Dokuchaev <edokuchaev@MacBook-Pro-mac.local>
2016-04-29 21:39:15 +0300

    models created

--- coworker/models.py
+++ coworker/models.py
@@ -1,12 +1,56 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
 # from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    github_account_id = models.CharField(max_length=256)
+    project = models.ForeignKey('Project')
+
+
+class ProjectOwner(models.Model):
+    profile = models.ForeignKey('Profile')
+    project = models.ForeignKey('Project')
+
+
+class ProjectHelper(models.Model):
+    profile = models.ForeignKey('Profile')
+    skill = models.ForeignKey('Skill')
+
+
+class Skill(models.Model):
+    skill_description = models.CharField(max_length=256)
+    level = models.IntegerField()
+
+
+class Project(models.Model):
+    skill = models.ForeignKey('Skill')
+    project_owner = models.ForeignKey('ProjectOwner')
+    project_helper = models.ForeignKey('ProjectHelper')
+    project_status = models.CharField(max_length=150)
+
+
+class Issue(models.Model):
+    project = models.ForeignKey('Project')
+    issue_status = models.CharField(max_length=150)
+    issue_link = models.CharField(max_length=150)
+    issue_title = models.CharField(max_length=150)
+    issue_description = models.CharField(max_length=150)
+    issue_label = models.CharField(max_length=150)
+
+
+class Milestone(models.Model):
+    project = models.ForeignKey('Project')
+    issue_status = models.CharField(max_length=150)
+    issue_link = models.CharField(max_length=150)
+    issue_title = models.CharField(max_length=150)
+    issue_description = models.CharField(max_length=150)
 
 
 # Create your models here.
 # class GithubAccount(models.Model):
-#     user = models.ForeignKey('auth.User')
+# user = models.ForeignKey('auth.User')
 #     login = models.CharField(max_length=50)
 #     email = models.CharField(max_length=50)
 #     token = models.CharField(max_length=50, blank=True, null=True)
