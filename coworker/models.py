8d0c86ae748744eb32103d8a08cb0eb0fb2d9595
guitarton <guitarton@yandex.ru>
2016-04-29 11:56:30 +0300

    login with pygithub

--- coworker/models.py
+++ coworker/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
-
 from django.db import models
+# from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+# class GithubAccount(models.Model):
+#     user = models.ForeignKey('auth.User')
+#     login = models.CharField(max_length=50)
+#     email = models.CharField(max_length=50)
+#     token = models.CharField(max_length=50, blank=True, null=True)
+#
+#     # repositories
+#     # issues
+#
+#
+#     def set_token(self):
+#         github_socialapp = SocialApp.objects.get(name='github app')
+#         current_socialaccount = SocialAccount.objects.get(user=self.user)
+#         current_token = SocialToken.objects.get(app=github_socialapp,
+#                                                 account=current_socialaccount)
+#         self.token = current_token
+#         self.save()
+#
+#     def set_email(self):
--- coworker/views.py
+++ coworker/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
+from github import Github
 
 
 # Create your views here.
-
 
 def index(request):
     return render(request, 'coworker/index.html', {})
@@ -14,9 +13,16 @@ def index(request):
 @login_required
 def profile(request):
     current_user = request.user
+
     github_socialapp = SocialApp.objects.get(name='github app')
     current_socialaccount = SocialAccount.objects.get(user=current_user)
     current_token = SocialToken.objects.get(app=github_socialapp,
                                             account=current_socialaccount)
-    print(current_user.id)
-    return render(request, 'coworker/profile.html', {'id': id})
+
+    github_user = Github(current_token)
+    params = {
+        'login': github_user.login,
+
+    }
+
+    return render(request, 'coworker/profile.html', {'params': params})
