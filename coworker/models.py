786b3b696dff34bb169879c8da6aae742d39c594
Evgeni Dokuchaev <edokuchaev@MacBook-Pro-mac.local>
2016-05-17 22:00:31 +0300

    test commit

--- coworker/models.py
+++ coworker/models.py
@@ -3,6 +3,38 @@ from django.db import models
 from django.utils import timezone
 # from django.contrib.auth.models import User
 
+# latest models:
+
+# class Profile(models.Model):
+#    github_account_id = models.CharField(max_length=256)
+#    is_active = models.BooleanField(default=True)
+#    skills = models.ForeignKey('Skill')
+#
+#
+# class Skill(models.Model):
+#    skill_description = models.CharField(max_length=256)
+#    level = models.IntegerField()
+#
+#
+# class Project(models.Model):
+#    skill = models.ForeignKey('Skill')
+#    project_owner = models.ForeignKey('Profile')
+#    project_helper = models.ManyToManyField('Profile')
+#    project_link = models.CharField(max_length=150)
+#    project_status = models.CharField(max_length=150)
+#    is_active = models.BooleanField(default=True)
+#
+#
+# class Issue(models.Model):
+#    project = models.ForeignKey('Project')
+#    issue_status = models.CharField(max_length=150)
+#    issue_link = models.CharField(max_length=150)
+#    issue_title = models.CharField(max_length=150)
+#    issue_description = models.CharField(max_length=150)
+#    issue_label = models.CharField(max_length=150)
+
+
+# previous version models:
 
 # class Profile(models.Model):
 #     github_account_id = models.CharField(max_length=256)
